9a199f95167a4769bef3b41066f64814cbb2637f
1hyphend <Ivanc221@nycstudents.net>
2025-10-27 09:28:24 -0400

    Update 123456789.py

--- 123456789.py
+++ 123456789.py
@@ -4,20 +4,23 @@ second=int(input(":  "))
 third=int(input(":  "))
 count = 0
 while quarters > 0:
+    print(count)
     first +=1
     quarters -=1
     count +=1
+    
     if first == 35:
         quarters +30
         first=0
-    
-    second+=1
+
+    second+=12
     quarters-=1
     count+=1
+
     if second==100:
         quarters += 60
         second=0
-    
+
     third +=1
     quarters -=1
     count+=1
