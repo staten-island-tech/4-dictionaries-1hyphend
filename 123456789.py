383678eb29b21caeaf160634460ebe5775661c1e
1hyphend <Ivanc221@nycstudents.net>
2025-10-27 10:08:46 -0400

    Update 123456789.py

--- 123456789.py
+++ 123456789.py
@@ -1,32 +1,32 @@
-quarters = int(input(":  "))
-first = int(input(":  "))
-second=int(input(":  "))
-third=int(input(":  "))
+quarters = int(input(":"))
+first= int(input(":"))
+second= int(input(":"))
+third= int(input(":"))
+
 count = 0
+
 while quarters > 0:
-    print(count)
-    first +=1
-    quarters -=1
+
+    first += 1
+    quarters -= 1
     count +=1
-    
     if first == 35:
-        quarters +30
-        first=0
+        quarters += 30
+        first = 0
 
-    second+=12
-    quarters-=1
-    count+=1
-
+    second += 1
+    quarters -= 1
+    count +=1
     if second==100:
         quarters += 60
-        second=0
-
-    third +=1
-    quarters -=1
-    count+=1
-
+        second = 0
+    if quarters==0:
+        break
+    third += 1
+    quarters -= 1
+    count += 1
     if third == 10:
         quarters += 9
-        third=0
+        third = 0
 
 print(f"She plays {count} times before going broke.")
