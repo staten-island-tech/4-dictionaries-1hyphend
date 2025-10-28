daf6130bc1a32fa8a924ee839a3346e2f27e1ed5
1hyphend <Ivanc221@nycstudents.net>
2025-10-28 10:17:16 -0400

    Update te.py

--- te.py
+++ te.py
@@ -1,2 +1,44 @@
-print("Welcome to Amazon, what are you tryna find boi")
+print("welcome to barnes and noble, what are you buying today")
 
+Barnes_noble_books = [
+{
+    "Title":"Dog Man Big Jim Believes",
+    "year": 2024,
+    "genre": "Fiction",
+    "price": 12.99,
+    "author": "Dav Pilkey"
+    
+}, 
+{
+    "Title":"The Bad Guys",
+    "year":2022,
+    "genre": "fiction",
+    "price": 22.99,
+    "author": "Aaron Blabey"
+
+}, 
+{
+    "Title":"The Bad Guys",
+    "year":2022,
+    "genre": "fiction",
+    "price": 22.99,
+    "author": "Aaron Blabey"
+},
+{
+    "Title":"Dog Man: A Graphic Novel",
+    "year":2022,
+    "genre": "fiction",
+    "price": 10.54,
+    "author": "Aaron Blabey"
+},
+{
+    "Title":"The Bad Guys in Let the Games Begin!",
+    "year":2023,
+    "genre": "fiction",
+    "price": 22.99,
+    "author": "Aaron Blabey"
+}
+]
+
+for index, Barnes_noble_books in enumerate(Barnes_noble_books):
+    print(index, ":", Barnes_noble_books["Title"])
