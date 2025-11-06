ebcbd7c35cfd187854a86e737bd1e83da707be1b
1hyphend <Ivanc221@nycstudents.net>
2025-11-06 09:33:59 -0500

    Update te.py

--- te.py
+++ te.py
@@ -1,8 +1,7 @@
-print("WELCOME TO BARNES AND NOBLES WE ONLY HAVE THESE BOOKS")
-cart = [ ]       
-total = 0
-shopping= [
-    {
+print("welcome to barnes and noble, here are the ONLY books we have")
+cart=[]
+BNBOOKS  = [
+{
     "Title":"Dog Man Big Jim Believes",
     "year": 2024,
     "genre": "Fiction",
@@ -40,21 +39,57 @@ shopping= [
     "author": "Aaron Blabey"
 }
 ]
-for index, item in enumerate(shopping):
-        print(index, ":", item["Title"])
-while shopping==True:
-    print(f"What books would you like to buy: 1. Dog Man: Big Jim believes 2. The Bad Guys 3. Dog man: A Tale of Two Kitties 4. Dog man: A graphic Noveln 5.The Bad Guys in Let the Games Begin!" )
-print (input("1,2,3,4,5"))
-choice = int(input("Enter the number of the item you wish to buy: "))
-selected_item = cart[choice]
-cart.append(shopping)
-total += selected_item["price"]
 
-answer = input("Do you want to continue shopping? (yes/no): ")
-if answer != "yes":
+for index, BNBOOKS in enumerate(BNBOOKS): 
+    print(index, ":", BNBOOKS["Title"])
+while True:
+    BNBOOKS > 0 
+    print("Is that all you want to buy ")
+    if BNBOOKS:
+
+        print(index("title"))
+        print(index("price"))
+# cart = [ ]       
+total = 0
+shopping = True
+while shopping:
+    print("Available items:")
+    for item in enumerate(store_items):
+        print(f"{item['name']}, {item['price']}, ({item['category']})")
+    choice = int(input("Enter the number of the item you wish to buy: "))
+    selected_item = store_items[choice]
+      cart.append(selected_item)
+    total += ["price"]
+        print(f”Added {selected_item['name']} to your cart.")
+    answer = input("Do you want to continue shopping? (yes/no): ")
+    if answer != "yes":
         shopping = False
-print(cart)
+print("cart")
 for item in cart:
-    print(f"-{item['title']}")
-print(f"Total:{total:.2f}")
+    print(f"- {item['name']}")
+print(f"Total: {total:.2f}")
+print("Thank you for shopping with us!")
+
+
+
+
+while True:
+    choice = input("What would you like to buy? (or type 'done' to finish): ")
+
+    if choice == 'done':
+        break  
+    if choice in store:
+        quantity = int(input(f"How many {books}s would you like to add? "))
+if cart:
+    print("Your Cart:")
+    total = 0
+    for item, quantity in cart.items():
+        cost = store[item] * quantity
+        total += cost
+        print(f"- {item()} x{quantity} = {cost:.2f}")
+
+    print(f" Your Total Amount is: {total:.2f}")
+else:
+    print("You have to buy something brokie.")
+
 print("Thank you for shopping with us!")
